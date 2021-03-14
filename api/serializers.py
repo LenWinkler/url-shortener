aa5a7dbe1bca1b9e9dd70af14b9e1619aee32a22
LenWinkler <lenwinkler86@gmail.com>
2021-03-14 16:16:40 -0400

    run migration, tweak create view

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Url
 
-class UrlSerializer(serializers.Serializer):
+class UrlSerializer(serializers.ModelSerializer):
     raw = serializers.URLField(max_length=250)
     url_hash = serializers.CharField(max_length=8)
     short = serializers.URLField()
+
+    class Meta:
+        model = Url
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 import hashlib
 from .serializers import UrlSerializer
 
@@ -10,18 +11,19 @@ def index(request):
 
 @api_view(['POST'])
 def create(request):
-    encoded_url = request.data['raw_url'].encode('utf-8')
+    encoded_url = request.data['raw'].encode('utf-8')
     hashed_url = hashlib.sha256(encoded_url).hexdigest()[:8]
 
     data_for_serializer = {
-        raw = request.data['raw'],
-        url_hash = hashed_url,
-        short = fr'http://127.0.0.1:8000/api/{url_hash}'
+        'raw': request.data['raw'],
+        'url_hash': hashed_url,
+        'short': fr'http://127.0.0.1:8000/{hashed_url}'
     }
 
     serializer = UrlSerializer(data=data_for_serializer)
 
     if serializer.is_valid():
         serializer.save()
-
-    return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

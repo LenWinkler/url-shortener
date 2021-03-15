7e62385cdb4ce1bc001135300efa87264eff219b
LenWinkler <lenwinkler86@gmail.com>
2021-03-15 01:29:45 -0400

    prevent duplicate hash values in db

---
+++ api/hash_checker.py
@@ -0,0 +1,8 @@
+from .models import Url
+
+def url_hash_exists(hashed_url):
+    try:
+        obj = Url.objects.get(url_hash=hashed_url)
+    except Url.DoesNotExist:
+        obj = None
+    return obj
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status
 import hashlib
 from .serializers import UrlSerializer
+from .hash_checker import url_hash_exists
 
 @api_view(['GET'])
 def index(request):
@@ -13,7 +14,12 @@ def index(request):
 def create(request):
     encoded_url = request.data['raw'].encode('utf-8')
     hashed_url = hashlib.sha256(encoded_url).hexdigest()[:8]
-
+    
+    already_exists = url_hash_exists(hashed_url)
+    if already_exists:
+        serializer = UrlSerializer(already_exists)  
+        return Response(serializer.data, status=status.HTTP_200_OK)
+        
     data_for_serializer = {
         'raw': request.data['raw'],
         'url_hash': hashed_url,
@@ -25,5 +31,5 @@ def create(request):
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

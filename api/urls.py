f7cedda2f572248cc819bb25e88e57c0502c70c6
LenWinkler <lenwinkler86@gmail.com>
2021-03-24 18:53:50 -0400

    remove info endpoint. empty path will be endpoint to create url

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api_info),
-    path('create/', views.create_url, name='create-url'),
+    path('', views.create_url, name='create-url'),
     path('<existing_hash>/', views.retrieve_url, name='retrieve-url'),
 ]
--- api/views.py
+++ api/views.py
@@ -10,15 +10,6 @@ from .hash_checker import url_hash_exists
 from .models import Url
 from .serializers import UrlSerializer
 
-
-@api_view(['GET'])
-def api_info(request):
-    return Response(
-        {"GET '/'": "Returns info about api",
-         "POST '/'": "Create url. Req body --> {'raw':'<full url>', 'custom'(optional):'<custom url>'}",
-         "GET '/<existing url>/'": "If url is valid, will redirect to the full length url"
-        })
-
 @api_view(['GET'])
 def retrieve_url(request, existing_hash):
     url_is_valid = url_hash_exists(existing_hash)
@@ -26,51 +17,69 @@ def retrieve_url(request, existing_hash):
         serializer = UrlSerializer(url_is_valid)
         return redirect(serializer.data['raw'])
     
-    return Response({'Error':'Url not found'}, status=status.HTTP_404_NOT_FOUND)
+    return Response(
+        {'Error':'Url not found'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def create_url(request):
-    data_for_serializer = {
-        'raw': request.data['raw'],
-        'url_hash': '',
-        'short': ''
-    }
+    try: # check to make sure 'raw' key is in req body
+        data_for_serializer = {
+            'raw': request.data['raw'],
+            'url_hash': '',
+            'short': ''
+        }
 
-    try: # if user passed a custom url in the request body
-        custom_url = request.data['custom']
+        try: # if user passed a custom url in the request body
+            custom_url = request.data['custom']
 
-        if not re.search(r'^[a-zA-Z0-9]{3,8}$', custom_url):
+            if not re.search(r'^[a-zA-Z0-9]{3,8}$', custom_url):
+                return Response(
+                    {
+                        'Error': 'Url must use only alphanumeric chars and' \
+                                 'must be between 3-8 chars'
+                    },
+                     status=status.HTTP_400_BAD_REQUEST)
+
+            custom_hash_already_exists = url_hash_exists(custom_url)
+            if custom_hash_already_exists:
+                return Response(
+                    {'Error': 'This url is already taken'},
+                     status=status.HTTP_409_CONFLICT)
+
+            data_for_serializer['url_hash'] = custom_url
+            data_for_serializer['short'] = f'http://127.0.0.1:8000/{custom_url}/'
+            serializer = UrlSerializer(data=data_for_serializer)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED)
+
             return Response(
-                {'Error': 'Url can only contain alphanumeric chars and must be between 3-8 chars long'},
-                 status=status.HTTP_400_BAD_REQUEST)
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        custom_hash_already_exists = url_hash_exists(custom_url)
-        if custom_hash_already_exists:
-            return Response({'Error': 'This url is already taken'}, status=status.HTTP_409_CONFLICT)
+        except: # if no custom url was passed
+            encoded_url = request.data['raw'].encode('utf-8')
+            hashed_url = hashlib.sha256(encoded_url).hexdigest()[:8]
 
-        data_for_serializer['url_hash'] = custom_url
-        data_for_serializer['short'] = f'http://127.0.0.1:8000/{custom_url}/'
-        serializer = UrlSerializer(data=data_for_serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            already_exists = url_hash_exists(hashed_url)
+            if already_exists:
+                serializer = UrlSerializer(already_exists)
+                return Response(
+                    {'already_exists': f'{serializer.data["short"]}'},
+                     status=status.HTTP_200_OK)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data_for_serializer['url_hash'] = hashed_url
+            data_for_serializer['short'] = f'http://127.0.0.1:8000/{hashed_url}/'
+            serializer = UrlSerializer(data=data_for_serializer)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(
+                    serializer.data, status=status.HTTP_201_CREATED)
 
-    except: # if no custom url was passed
-        encoded_url = request.data['raw'].encode('utf-8')
-        hashed_url = hashlib.sha256(encoded_url).hexdigest()[:8]
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        already_exists = url_hash_exists(hashed_url)
-        if already_exists:
-            serializer = UrlSerializer(already_exists)
-            return Response({'already_exists': f'{serializer.data["short"]}'}, status=status.HTTP_200_OK)
-
-        data_for_serializer['url_hash'] = hashed_url
-        data_for_serializer['short'] = f'http://127.0.0.1:8000/{hashed_url}/'
-        serializer = UrlSerializer(data=data_for_serializer)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response(
+                        {"Error": 'Missing "raw" key in body of request'},
+                         status=status.HTTP_400_BAD_REQUEST)

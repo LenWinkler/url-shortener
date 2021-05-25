2a56c0f8293974fd34bb8de5b5407c08fde9d9fe
LenWinkler <lenwinkler86@gmail.com>
2021-05-25 01:01:41 -0400

    fix f strings in views

--- api/views.py
+++ api/views.py
@@ -23,7 +23,7 @@ def retrieve_url(request, existing_hash):
 @api_view(['POST', 'GET'])
 def create_url(request):
     try: # check to make sure 'raw' key is in req body
-        data_for_serializer = {
+        serializer_data = {
             'raw': request.data['raw'],
             'url_hash': '',
             'short': ''
@@ -38,22 +38,22 @@ def create_url(request):
                         'Error': 'Url must use only alphanumeric chars and' \
                                  'must be between 3-8 chars'
                     },
-                     status=status.HTTP_400_BAD_REQUEST)
+                    status=status.HTTP_400_BAD_REQUEST)
 
             custom_hash_already_exists = url_hash_exists(custom_url)
             if custom_hash_already_exists:
                 return Response(
                     {'Error': 'This url is already taken'},
-                     status=status.HTTP_409_CONFLICT)
+                    status=status.HTTP_409_CONFLICT)
 
-            data_for_serializer['url_hash'] = custom_url
-            data_for_serializer['short'] = f'https://shortn-it.herokuapp.com' \
-                                            '/{custom_url}/'
-            serializer = UrlSerializer(data=data_for_serializer)
+            serializer_data['url_hash'] = custom_url
+            serializer_data['short'] = (f'''https://shortn-it.herokuapp.com'''
+                                        f'''/{custom_url}/''')
+            serializer = UrlSerializer(data=serializer_data)
             if serializer.is_valid():
                 serializer.save()
-                return Response(
-                    serializer.data, status=status.HTTP_201_CREATED)
+                return Response(serializer.data, 
+                                status=status.HTTP_201_CREATED)
 
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -67,12 +67,12 @@ def create_url(request):
                 serializer = UrlSerializer(already_exists)
                 return Response(
                     {'already_exists': f'{serializer.data["short"]}'},
-                     status=status.HTTP_200_OK)
+                    status=status.HTTP_200_OK)
 
-            data_for_serializer['url_hash'] = hashed_url
-            data_for_serializer['short'] = f'https://shortn-it.herokuapp.com' \
-                                            '/{hashed_url}/'
-            serializer = UrlSerializer(data=data_for_serializer)
+            serializer_data['url_hash'] = hashed_url
+            serializer_data['short'] = (f'''https://shortn-it.herokuapp.com'''
+                                        f'''/{hashed_url}/''')
+            serializer = UrlSerializer(data=serializer_data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(
@@ -84,4 +84,4 @@ def create_url(request):
     except:
         return Response(
                         {"Error": 'Missing "raw" key in body of request'},
-                         status=status.HTTP_400_BAD_REQUEST)
+                        status=status.HTTP_400_BAD_REQUEST)
